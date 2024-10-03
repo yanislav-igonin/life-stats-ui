b3de7d63ff9d6e05fea85a6db83a51f8a948c648
Yanislav Igonin <yanislavdabest@gmail.com>
2024-10-03 14:12:08 +0300

    Fix - Redirect on auth

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -3,12 +3,20 @@ import { config } from "./config";
 
 export const httpClientUnauthed = ky.create({
 	prefixUrl: config.apiUrl,
+	throwHttpErrors: true,
 });
 
 export const httpClient = ky.create({
 	prefixUrl: config.apiUrl,
-	headers: {
-		Authorization: localStorage.getItem("authToken") ?? "",
-	},
 	throwHttpErrors: true,
+	hooks: {
+		beforeRequest: [
+			async (request) => {
+				const authToken = localStorage.getItem("authToken");
+				if (authToken) {
+					request.headers.set("Authorization", authToken);
+				}
+			},
+		],
+	},
 });
